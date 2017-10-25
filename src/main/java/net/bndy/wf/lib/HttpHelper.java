1b250755549232d246334ff2cb9812d572b7b7a1
Bendy Zhang <zb@bndy.net>
2017-10-25 21:33:15 +0800

    http tools

---
+++ src/main/java/net/bndy/wf/lib/HttpHelper.java
@@ -0,0 +1,174 @@
+/*******************************************************************************
+ * Copyright (C) 2017 http://bndy.net
+ * Created by Bendy (Bing Zhang)
+ ******************************************************************************/
+package net.bndy.wf.lib;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HttpHelper {
+
+	public static String requestGet(String url) throws Exception {
+		return requestGet(url, null, null);
+	}
+	
+	public static String requestGet(String url, URLConnection connection) throws Exception {
+		return requestGet(url, connection, "utf-8");
+	}
+
+	public static String requestGet(String url, URLConnection connection, String charset) throws Exception {
+
+		if (connection == null) {
+			connection = openConnection(url);
+		}
+		
+		if (charset == null || charset == "") {
+			charset = "utf-8";
+		}
+		
+		HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+
+		httpURLConnection.setRequestProperty("Accept-Charset", charset);
+		httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+		InputStream inputStream = null;
+		InputStreamReader inputStreamReader = null;
+		BufferedReader reader = null;
+		StringBuffer resultBuffer = new StringBuffer();
+		String tempLine = null;
+
+		try {
+			inputStream = httpURLConnection.getInputStream();
+			inputStreamReader = new InputStreamReader(inputStream);
+			reader = new BufferedReader(inputStreamReader);
+
+			while ((tempLine = reader.readLine()) != null) {
+				resultBuffer.append(tempLine);
+			}
+
+		} finally {
+
+			if (reader != null) {
+				reader.close();
+			}
+
+			if (inputStreamReader != null) {
+				inputStreamReader.close();
+			}
+
+			if (inputStream != null) {
+				inputStream.close();
+			}
+		}
+
+		return resultBuffer.toString();
+	}
+
+	public static String requestPost(String url, Map<?, ?> parameterMap) throws Exception {
+		StringBuffer parameterBuffer = new StringBuffer();
+		if (parameterMap != null) {
+			Iterator<?> iterator = parameterMap.keySet().iterator();
+			String key = null;
+			String value = null;
+			while (iterator.hasNext()) {
+				key = (String) iterator.next();
+				if (parameterMap.get(key) != null) {
+					value = (String) parameterMap.get(key);
+				} else {
+					value = "";
+				}
+
+				parameterBuffer.append(key).append("=").append(value);
+				if (iterator.hasNext()) {
+					parameterBuffer.append("&");
+				}
+			}
+		}
+
+		URLConnection connection = openConnection(url);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+
+		httpURLConnection.setDoOutput(true);
+		httpURLConnection.setRequestMethod("POST");
+		httpURLConnection.setRequestProperty("Accept-Charset", "UTF-8");
+		httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		httpURLConnection.setRequestProperty("Content-Length", String.valueOf(parameterBuffer.length()));
+
+		OutputStream outputStream = null;
+		OutputStreamWriter outputStreamWriter = null;
+		InputStream inputStream = null;
+		InputStreamReader inputStreamReader = null;
+		BufferedReader reader = null;
+		StringBuffer resultBuffer = new StringBuffer();
+		String tempLine = null;
+
+		try {
+			outputStream = httpURLConnection.getOutputStream();
+			outputStreamWriter = new OutputStreamWriter(outputStream);
+
+			outputStreamWriter.write(parameterBuffer.toString());
+			outputStreamWriter.flush();
+
+			inputStream = httpURLConnection.getInputStream();
+			inputStreamReader = new InputStreamReader(inputStream);
+			reader = new BufferedReader(inputStreamReader);
+
+			while ((tempLine = reader.readLine()) != null) {
+				resultBuffer.append(tempLine);
+			}
+
+		} finally {
+
+			if (outputStreamWriter != null) {
+				outputStreamWriter.close();
+			}
+
+			if (outputStream != null) {
+				outputStream.close();
+			}
+
+			if (reader != null) {
+				reader.close();
+			}
+
+			if (inputStreamReader != null) {
+				inputStreamReader.close();
+			}
+
+			if (inputStream != null) {
+				inputStream.close();
+			}
+
+		}
+
+		return resultBuffer.toString();
+	}
+
+	public static URLConnection openConnection(String urlString) throws IOException {
+		return openConnection(urlString, null, null);
+	}
+	
+	public static URLConnection openConnection(String urlString, String proxyHost, Integer proxyPort) throws IOException {
+		URL url = new URL(urlString);
+		URLConnection connection;
+		if (proxyHost != null && proxyPort != null) {
+			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+			connection = url.openConnection(proxy);
+		} else {
+			connection = url.openConnection();
+		}
+		return connection;
+	}
+}
--- src/test/java/net/bndy/wf/test/UtilsTest.java
+++ src/test/java/net/bndy/wf/test/UtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import net.bndy.wf.lib.StringHelper;
+import net.bndy.wf.lib.HttpHelper;
 
 public class UtilsTest extends _Test {
 	
@@ -12,5 +13,17 @@ public class UtilsTest extends _Test {
 		System.out.println(StringHelper.generateRandomString(11));
 		System.out.println(StringHelper.generateUUID());
 		Assert.assertEquals(StringHelper.generateRandomString(10).length(), 10);
+	}
+	
+	@Test
+	public void httpGet() throws Exception {
+		String s = HttpHelper.requestGet("http://www.hashcollision.org/hkn/python/idle_intro/index.html");
+		System.out.println(s);
+		Assert.assertTrue(s.indexOf("<!DOCTYPE") == 0);
+	}
+	
+	@Test
+	public void httpPost() throws Exception {
+		// TODO
 	}
 }
