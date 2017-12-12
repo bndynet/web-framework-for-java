3ead1181f06918429b3c585a1251569184ed5275
Bendy Zhang <zb@bndy.net>
2017-12-12 19:36:49 +0800

    feat(app): cache for i18n resource (#18)

--- src/main/java/net/bndy/wf/modules/app/api/I18nController.java
+++ src/main/java/net/bndy/wf/modules/app/api/I18nController.java
@@ -4,15 +4,16 @@
  ******************************************************************************/
 package net.bndy.wf.modules.app.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import net.bndy.wf.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import pl.jalokim.propertiestojson.util.PropertiesToJsonParser;
 
+import javax.cache.annotation.CacheResult;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,11 +24,12 @@ import java.util.Map;
 public class I18nController {
 
     @ApiOperation(value = "Gets all messages of current locale")
+    @CacheResult(cacheName = "default")
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public Object lang() {
+    public Object lang(@RequestParam(name = "lang") String lang) {
         Map<String, String> map = new HashMap<>();
         Enumeration<String> enumeration = ApplicationContext.i18n().getKeys();
-        for(; enumeration.hasMoreElements();) {
+        for (; enumeration.hasMoreElements(); ) {
             String key = enumeration.nextElement();
             map.put(key, ApplicationContext.i18n().getString(key));
         }
