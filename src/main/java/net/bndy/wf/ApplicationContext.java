12fd5e5cc1823d351a67fe588b14e8777fa46602
Bendy Zhang <zb@bndy.net>
2017-12-14 20:45:11 +0800

    feat: use MessageSource instead of ResourceBundle for language

--- src/main/java/net/bndy/wf/ApplicationContext.java
+++ src/main/java/net/bndy/wf/ApplicationContext.java
@@ -4,15 +4,30 @@
  ******************************************************************************/
 package net.bndy.wf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import net.bndy.wf.modules.app.models.User;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.*;
 
+@Component
 public class ApplicationContext {
+
+    private static MessageSource messageSource;
+
+    @Autowired
+    private MessageSource _messageSource;
+
+    @PostConstruct
+    private void init() {
+        messageSource = _messageSource;
+    }
 
     public static User getCurrentUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -25,5 +40,17 @@ public class ApplicationContext {
 
     public static ResourceBundle i18n() {
         return ResourceBundle.getBundle("i18n.messages", LocaleContextHolder.getLocale());
+    }
+
+    public static String language(String key, Object[] args, String defaultValue) {
+        return messageSource.getMessage(key, args, defaultValue, LocaleContextHolder.getLocale());
+    }
+
+    public static String language(String key, Object[] args) {
+        return language(key, args, null);
+    }
+
+    public static String language(String key) {
+        return language(key, null);
     }
 }
