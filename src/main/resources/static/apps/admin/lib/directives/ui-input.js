a0c78c65e8095e5a9f54258a939d9d0ef348653c
Bendy Zhang <zb@bndy.net>
2018-01-12 19:47:31 +0800

    feat(angularjs): uiInput component for packaging form-group

---
+++ src/main/resources/static/apps/admin/lib/directives/ui-input.js
@@ -0,0 +1,43 @@
+app.directive('uiInput', ['$translate', function($translate){
+    return {
+        restrict: 'E',
+        replace: true,
+        require: ['ngModel'],
+        scope: {
+            name: '@',
+            label: '@',
+            type: '@',
+            placeholder: '@',
+            tips: '@',
+            prefix: '@',
+            suffix: '@',
+            model: '=ngModel',
+            required: '=?',
+            hasSuccess: '=?',
+            successMessage: '@',
+            hasWarning: '=?',
+            warningMessage: '@',
+            hasError: '=?',
+            errorMessage: '@',
+        },
+        templateUrl: '/static/apps/admin/lib/directives/ui-input.html',
+        link: function(scope, elem, attrs) {
+            if (scope.required) {
+                if (!scope.errorMessage) {
+                    scope.errorMessage = 'common.msgRequired';
+                }
+            }
+
+            if (scope.prefix && scope.prefix.indexOf('class:') === 0) {
+                scope.prefixIcon = scope.prefix.replace('class:', '').trim();
+            }
+            if (scope.suffix && scope.suffix.indexOf('class:') === 0) {
+                scope.suffixIcon = scope.suffix.replace('class:', '').trim();
+            }
+
+            scope.isFormInvalid = function () {
+                return scope.fieldForm[scope.name].$dirty && scope.fieldForm[scope.name].$invalid;
+            };
+        }
+    };
+}]);
