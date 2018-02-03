8b0732805ccb0ce53aea700dc50b2f059e9baa7a
Bendy Zhang <zb@bndy.net>
2018-02-03 15:41:19 +0800

    feat(angularjs): component uiDialog

--- src/main/resources/static/apps/admin/lib/directives/ui-dialog.js
+++ src/main/resources/static/apps/admin/lib/directives/ui-dialog.js
@@ -8,10 +8,21 @@ angular.module('app')
                 transclude: {
                     footer: '?uiDialogFooter'
                 },
-                scope: true,
+                scope: {
+                    name: '@',
+                    title: '@'
+                },
                 templateUrl: '/static/apps/admin/lib/directives/ui-dialog.html',
                 link: function(scope, elem, attrs, ctrl, $transclude) {
                     scope.hasFooter = $transclude.isSlotFilled('footer');
+                    // NOTE: the following code is to share the controller scope for body and footer instead of ng-transclude
+                    $transclude(scope.$parent, function(content) {
+                        elem.find('.modal-body').append(content);
+                    });
+                    $transclude(scope.$parent, function(content) {
+                        var elemFooter = elem.find('.modal-footer');
+                        elem.find('.modal-footer').append(content);
+                    }, null, "footer");
                 }
             }
         }
