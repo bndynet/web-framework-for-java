ad3cd514ae0c4ae22b4d96659661dfc4900054db
Bendy Zhang <zb@bndy.net>
2018-02-11 19:05:39 +0800

    feat(angularjs): add switches for each icon buttons in uiActions

--- src/main/resources/static/apps/admin/lib/directives/ui-actions.js
+++ src/main/resources/static/apps/admin/lib/directives/ui-actions.js
@@ -7,13 +7,39 @@ angular.module('app')
                 scope: {
                     model: '=ngModel',
                     onView: '&?',
+                    showView: '=?',
                     onAdd: '&?',
+                    showAdd: '=?',
                     onEdit: '&?',
+                    showEdit: '=?',
                     onRemove: '&?',
+                    showRemove: '=?',
                     onToggleVisible: '&?',
+                    showToggleVisible: '=?',
                     onExchange: '&?',
+                    showExchange: '=?',
                 },
                 templateUrl: '/static/apps/admin/lib/directives/ui-actions.html',
+                link: function(scope, elem, attrs) {
+                    if (typeof scope.showView === 'undefined') {
+                        scope.showView = !!scope.onView;
+                    }
+                    if (typeof scope.showAdd === 'undefined') {
+                        scope.showAdd = !!scope.onAdd;
+                    }
+                    if (typeof scope.showEdit === 'undefined') {
+                        scope.showEdit = !!scope.onEdit;
+                    }
+                    if (typeof scope.showRemove === 'undefined') {
+                        scope.showRemove = !!scope.onRemove;
+                    }
+                    if (typeof scope.showToggleVisible === 'undefined') {
+                        scope.showToggleVisible = !!scope.onToggleVisible;
+                    }
+                    if (typeof scope.showExchange === 'undefined') {
+                        scope.showExchange = !!scope.onExchange;
+                    }
+                },
             };
         }
     ]);
