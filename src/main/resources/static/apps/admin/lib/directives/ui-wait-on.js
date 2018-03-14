13084de5d93f4aa98a435b284c9216717cf77d61
Bendy Zhang <zb@bndy.net>
2018-03-14 20:18:31 +0800

    style(angularjs): close #52 - Beautify waiting appearance for buttons with uiWaitOn

--- src/main/resources/static/apps/admin/lib/directives/ui-wait-on.js
+++ src/main/resources/static/apps/admin/lib/directives/ui-wait-on.js
@@ -9,7 +9,7 @@ angular.module('app')
                 scope.$watch(attr.uiWaitOn, function (value) {
                     elem.attr('disabled', value);
                     if (value) {
-                        elem.prepend('<span class="in-process"><i class="fa fa-spin fa-spinner"></i></span>');
+                        elem.prepend('<span class="in-process"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></span>');
                         angular.forEach(children, function(child) {
                             angular.element(child).css('visibility', 'hidden');
                         });
